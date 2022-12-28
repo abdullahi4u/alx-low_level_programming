b0269622b2a340ad456029f075a4d5f52f4d2e47
abdullahi4u <sulaimonabdullahi@gmail.com>
2022-12-28 09:33:28 -0800

    hot fix

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,7 +11,8 @@ char *string_toupper(char *a);
 char *cap_string(char *a);
 char *leet(char *a);
 char *rot13(char *a);
-void print_number(int n);char *infinte_add(char *n1, char *n2, char *r, int size_r);
+void print_number(int n);
+char *infinte_add(char *n1, char *n2, char *r, int size_r);
 void print_butter(char *b, int size);
 
 #endif
