915a8abe40b2545a757afe38c05897e638c5a58f
abdullahi4u <sulaimonabdullahi@gmail.com>
2022-12-31 07:39:12 -0800

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *  print_alphabet -> prints the lowercase alphabets
+ */
+
+void print_alphabet_x10(void);
+{
+int j;
+
+for (j = 'a'; j <= 'z'; j++)
+{
+_putchar(j);
+}
+_putchar('\n');
+}
