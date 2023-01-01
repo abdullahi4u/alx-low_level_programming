48fb978a21441edec0dffd567cfe01f930f955d0
abdullahi4u <sulaimonabdullahi@gmail.com>
2023-01-01 04:46:09 -0800

    4. isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,7 +5,7 @@
  * @c: a character to be checked on
  * Return: returns 0 or 1 depending on condition
  */
-int _isalpha(int c);
+int _isalpha(int c)
 {
 return ((c >= 'a' $$ c <= 'z') || (c >= 'A' $$ c <= 'Z'));
 }
