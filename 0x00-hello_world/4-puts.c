1309c606b6e1151a259cefd46481394cca6df05d
abdullahi4u <sulaimonabdullahi@gmail.com>
2023-03-04 09:02:24 -0800

    hot fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,9 @@
  */
 int main(void)
 {
- puts("\"Programming is like building a multilingual puzzle");
- return (0);
+
+       	puts("\"Programming is like building a multilingual puzzle");
+
+       	return (0);
 }
-  
+
