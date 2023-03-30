85660203e1f16674cc742c1943bc82a7a73b34ed
abdullahi4u <sulaimonabdullahi@gmail.com>
2023-03-30 07:19:30 -0700

    hot fix

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,4 +1,3 @@
-
 #include <stdlib.h>
 #include "lists.h"
 
@@ -10,14 +9,14 @@
  */
 size_t list_len(const list_t *h)
 {
- size_t n = 0;
+size_t n = 0;
 
- while (h)
- {
- n++;
- h = h->next;
- }
- return (n);
+while (h)
+{
+n++;
+h = h->next;
+}
+return (n);
 }
 
 
