33bd6b6b4eeb715de5a3884026caef208197d8e3
abdullahi4u <sulaimonabdullahi@gmail.com>
2023-03-31 06:59:40 -0700

    hot fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,7 +9,6 @@
  */
 
 char *str_concat(char *s1, char *s2)
-
 {
 char *conct;
 
@@ -26,45 +25,29 @@ s2 = "";
 i = ci = 0;
 
 while (s1[i] != '\0')
-
 i++;
-
 while (s2[ci] != '\0')
-
 ci++;
-
 conct = malloc(sizeof(char) * (i + ci + 1));
-
 if (conct == NULL)
-
 return (NULL);
 
 i = ci = 0;
-
 while (s1[i] != '\0')
-
 {
 
 conct[i] = s1[i];
-
 i++;
-
 }
 
 while (s2[ci] != '\0')
-
 {
 
 conct[i] = s2[ci];
-
 i++, ci++;
-
 }
 
 conct[i] = '\0';
-
 return (conct);
 }
-
-
 
