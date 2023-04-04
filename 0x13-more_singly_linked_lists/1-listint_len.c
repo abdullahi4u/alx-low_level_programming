ac2379271c7b796d81de54e5ed342a44671105ad
abdullahi4u <sulaimonabdullahi@gmail.com>
2023-04-04 13:53:06 -0700

    task 1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -8,14 +8,14 @@
  */
 size_t listint_len(const listint_t *h)
 {
- size_t num = 0;
+size_t num = 0;
 
- while (h)
- {
- num++;
- h = h->next;
- }
+while (h)
+{
+num++;
+h = h->next;
+}
 
- return (num);
+return (num);
 }
 
