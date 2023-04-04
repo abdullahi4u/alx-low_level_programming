c4802818b91eade6961ecdf361d11789502616d5
abdullahi4u <sulaimonabdullahi@gmail.com>
2023-04-04 14:24:00 -0700

    task 4

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -6,13 +6,13 @@
  */
 void free_listint(listint_t *head)
 {
- listint_t *temp;
+listint_t *temp;
 
- while (head)
- {
- temp = head->next;
- free(head);
- head = temp;
- }
+while (head)
+{
+temp = head->next;
+free(head);
+head = temp;
+}
 }
 
