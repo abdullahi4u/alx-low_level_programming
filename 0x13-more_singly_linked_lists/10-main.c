9ea4f1c455eb2542bad367ca04d830daf0d7b832
abdullahi4u <sulaimonabdullahi@gmail.com>
2023-04-05 19:58:57 -0700

    hot fix task 10

--- 0x13-more_singly_linked_lists/10-main.c
+++ 0x13-more_singly_linked_lists/10-main.c
@@ -8,9 +8,11 @@
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
 listint_t *head;
+
 head = NULL;
 add_nodeint_end(&head, 0);
 add_nodeint_end(&head, 1);
@@ -43,7 +45,6 @@ printf("-----------------\n");
 delete_nodeint_at_index(&head, 0);
 print_listint(head);
 printf("-----------------\n");
-
 delete_nodeint_at_index(&head, 0);
 printf("-----------------\n");
 delete_nodeint_at_index(&head, 0);
@@ -64,5 +65,6 @@ delete_nodeint_at_index(&head, 0);
 printf("-----------------\n");
 delete_nodeint_at_index(&head, 0);
 print_listint(head);
+
 return (0);
 }
